46c4cd8ee5545549a258e6c09c4901b95c6c2907
henriqueclaranhan <henrique.claranhan@outlook.com>
2022-06-17 00:17:14 -0400

    prevent the user from clicking the button again

---
+++ src/main/resources/static/scripts/goodbyeButton.js
@@ -0,0 +1,14 @@
+const contentSayGoodbyeP = document.querySelector("content > p");
+const sayGoodByeForm = document.querySelector("#say-goodbye-form");
+const sayGoodByeButton = document.querySelector("#say-goodbye-button");
+
+const userSaidGoodbye = localStorage.getItem("userSaidGoodbye");
+
+if (userSaidGoodbye != null) {
+	contentSayGoodbyeP.innerHTML = contentSaidGoodbyePText;
+	sayGoodByeForm.remove()
+}
+
+sayGoodByeButton.addEventListener("click", () => {
+	localStorage.setItem("userSaidGoodbye", "yes");
+}, false);
