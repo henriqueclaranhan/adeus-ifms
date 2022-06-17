a46313a079adbcf54fd70971b8199b5af8c082e5
henriqueclaranhan <henrique.claranhan@outlook.com>
2022-06-16 22:25:44 -0400

    remove unused imports

--- src/main/java/com/example/trabaio/controller/IndexController.java
+++ src/main/java/com/example/trabaio/controller/IndexController.java
@@ -1,14 +1,10 @@
 package com.example.trabaio.controller;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.trabaio.repository.AdeusRepository;
 import com.example.trabaio.orm.Adeus;
--- src/main/java/com/example/trabaio/orm/Adeus.java
+++ src/main/java/com/example/trabaio/orm/Adeus.java
@@ -1,18 +1,9 @@
 package com.example.trabaio.orm;
 
-import java.util.Date;
-
-import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Adeus {
